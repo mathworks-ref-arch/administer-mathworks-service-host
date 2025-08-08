ecbf03adb7d788b77d0f30b92bc146d19e0fcbf6
Dimitrios Kikidis <dkikidis@mathworks.com>
2025-08-08 14:30:25 +0100

    Extend the sysprep cleanup script to the `Default` user profile too

--- admin-scripts/windows/README.md
+++ admin-scripts/windows/README.md
@@ -2,7 +2,7 @@
 
 This folder includes scripts that you can use to:
 - Install MathWorks&reg; Service Host in a custom location. For details, see the [admin-controlled-installation folder](./admin-controlled-installation/).  
-- Clean up all MathWorks Service Host data in `%LOCALAPPDATA%\MathWorks` before running sysprep. For details, see the [sysprep folder](./sysprep/).
+- Clean up all MathWorks Service Host data in `%LOCALAPPDATA%\MathWorks` before running `sysprep`. For details, see the [sysprep folder](./sysprep/).
 
 ----
 
--- admin-scripts/windows/sysprep/cleanup_msh_data_before_sysprep.ps1
+++ admin-scripts/windows/sysprep/cleanup_msh_data_before_sysprep.ps1
@@ -3,8 +3,10 @@
 # Exit if any command fails
 $ErrorActionPreference = "Stop"
 
+
 # Stop any processes which may be using MathWorks Service Host
 $allProcessNames = @("MATLAB", "MATLABConnector", "MathWorksServiceHost")
+$remainingProcesses = @()
 foreach ($processName in $allProcessNames) {
     $proceses = Get-WmiObject Win32_Process | Where-Object { $_.Name -like "*$processName*" }
     foreach ($process in $proceses) {
@@ -12,28 +14,77 @@ foreach ($processName in $allProcessNames) {
             $process.Terminate() | Out-Null
         } catch {
             if (-not (($_.CategoryInfo.Category -eq "ObjectNotFound") -or ($_.CategoryInfo.Category -eq "NotSpecified"))) {
-                Write-Error "Could not stop the process: $processName"
-                throw;
+                $remainingProcesses += "$processName"
             }
         }
     }
 }
 
-$LocalAppData = "$env:LOCALAPPDATA"
-$AppData = "$env:APPDATA"
+if ($remainingProcesses.Count -gt 0) {
+    $remainingProcessesList = $remainingProcesses -join "`n`t"
+    throw "Could not stop the processes:`n`t$remainingProcessesList"
+}
 
-# Clean up all user specific directories
-$directories= @("$LocalAppData\MathWorks\ServiceHost",
-                "$LocalAppData\MathWorks\MATLABConnector",
-                "$LocalAppData\MathWorks\mwEndpointRegistry",
-                "$AppData\MathWorks\licensing",
-                "$AppData\MathWorks\credentials")
+$defaultProfile = Join-Path $env:SystemDrive 'Users\Default'
+$defaultLocalAppData = Join-Path $defaultProfile 'AppData\Local'
+$defaultAppData = Join-Path $defaultProfile 'AppData\Roaming'
+
+
+$profileDirectories = @{
+    LocalAppData = @(
+        "$env:LOCALAPPDATA",
+        $defaultLocalAppData
+    )
+    AppData = @(
+        "$env:APPDATA",
+        $defaultAppData
+    )
+}
+
+$subdirectories = @{
+    LocalAppData = @(
+        'MathWorks\ServiceHost',
+        'MathWorks\MATLABConnector',
+        'MathWorks\mwEndpointRegistry'
+    )
+    AppData = @(
+        'MathWorks\licensing',
+        'MathWorks\credentials'
+    )
+}
+
+$directories = @()
+foreach ($base in $profileDirectories.LocalAppData) {
+    foreach ($sub in $subdirectories.LocalAppData) {
+        $directories += Join-Path $base $sub
+    }
+}
+foreach ($base in $profileDirectories.AppData) {
+    foreach ($sub in $subdirectories.AppData) {
+        $directories += Join-Path $base $sub
+    }
+}
+
+$remainingDirectories = @()
 foreach ($directory in $directories) {
     try {
         Remove-Item -Path $directory -Recurse -Force -ErrorAction Stop
     } catch {
         if (-not ($_.CategoryInfo.Category -eq "ObjectNotFound")) {
-            throw;
+            $remainingDirectories += "$directory"
         }
     }
 }
+
+if ($remainingDirectories.Count -gt 0) {
+    $remainingDirectoriesList = $remainingDirectories -join "`n`t"
+    throw "Could not remove the directories:`n`t$remainingDirectoriesList"
+}
+
+$M = ""
+$M += "MathWorks Service Host is cleaned up, and you can now run ``sysprep``. `n"
+$M += "Please note that any use of MathWorks product before ``sysprep`` will revert the cleanup, requiring that this script is run again. `n"
+
+Write-Host $M
+
+exit 0
